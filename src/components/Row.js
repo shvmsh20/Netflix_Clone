ea339d51ee59a11e77fae45e055e6c2f32aba90e
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-06 23:43:48 +0530

    add rows

---
+++ src/components/Row.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from "react";
+import axios from "../axios";
+import "./Row.css";
+
+const base_URL = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({title, fetchUrl, isLargeRow})=>{
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl])
+
+
+    return <div className="row">
+        <h2>{title}</h2>
+        <div className="row_posters">
+            {movies.map(movie=>(
+                <img 
+                key={movie.id}
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                 src={`${base_URL}${isLargeRow?movie.poster_path:movie.backdrop_path}`} 
+                alt={movie.name} />
+            ))}
+        </div>
+    </div>
+}
+
+export default Row;
